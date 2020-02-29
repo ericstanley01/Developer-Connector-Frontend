a56776b696db5b9bc19eaf0fc409cf7f7ba79bb1
eric.stanleyjones@gmail.com <eric.stanleyjones@gmail.com>
2020-02-29 13:25:36 +0530

    Completed create profile actions and UI component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import jwtDecode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
 import { setCurrentUser, logoutUser } from './actions/authActions';
 import { Provider } from 'react-redux';
 import store from './store';
 
+import PrivateRoute from './components/common/PrivateRoute';
+
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Footer from './components/layout/Footer';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/create-profile/CreateProfile';
 
 import './App.css';
+import { clearCurrentProfile } from './actions/profileActions';
 
 // check for token
 if (localStorage.jwtToken) {
@@ -26,9 +31,8 @@ if (localStorage.jwtToken) {
   const currentTime = Date.now() / 1000;
   if (decodedToken.exp < currentTime) {
     // logout user
-    store.dispatch(logoutUser);
-    // todo: clear current profile
-
+    store.dispatch(logoutUser());
+    store.dispatch(clearCurrentProfile());
     // redirect to login
     window.location.href = '/login';
   }
@@ -45,6 +49,12 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register}></Route>
               <Route exact path="/login" component={Login}></Route>
+              <Switch>
+                <PrivateRoute exact path="/dashboard" component={Dashboard}></PrivateRoute>
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/create-profile" component={CreateProfile}></PrivateRoute>
+              </Switch>
             </div>
             <Footer></Footer>
           </div>
