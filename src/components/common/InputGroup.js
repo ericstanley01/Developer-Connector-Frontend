15196c5cd214310d98d1f8f592d51fea3db11fa5
eric.stanleyjones@gmail.com <eric.stanleyjones@gmail.com>
2020-02-29 16:24:21 +0530

    Added bootstrap superhero theme

--- src/components/common/InputGroup.js
+++ src/components/common/InputGroup.js
@@ -14,7 +14,7 @@ function InputGroup({
  return (
   <div className="input-group mb-3">
    <div className="input-group-prepend">
-    <span className="input-group-text mr-2">
+    <span className="input-group-text">
      <i className={icon}></i>
     </span>
    </div>
